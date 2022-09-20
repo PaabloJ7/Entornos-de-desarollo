6c3a8123c095ba28f229212b5c45825c3d47f44f
PaabloJ7 <113978289+PaabloJ7@users.noreply.github.com>
2022-09-20 10:55:59 +0200

    Hola mundo

---
+++ HolaMundo.java
@@ -0,0 +1,15 @@
+/**
+ * Muestra por pantalla la frase "¡Hola Mundo!"
+ * 
+ * @author
+ * 
+ */
+ 
+
+ public class HolaMundo {  // Clase principal
+    
+    public static void main (String [] args) {
+      System.out.println("¡HolaMundo!");
+    }
+}
+[
